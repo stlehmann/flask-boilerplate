c78b9a8db2f352f442fbc872bec9456379719de2
MrLeeh <Stefan.St.Lehmann@gmail.com>
2016-07-21 13:48:22 +0200

    set up the main files

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'my secret key'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig,
+    'testing': TestingConfig
+}
---
+++ manage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+from app import create_app, db
+from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
+
+# import specific models for use in shell
+# from app.models import *
+
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(app=app, db=db)
+
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """
+    Run unit tests
+
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
